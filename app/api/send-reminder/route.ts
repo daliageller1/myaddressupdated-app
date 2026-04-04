69e77f68d5433d6010b4cac7b1e47f282b0739f3
Dalia Geller <daliageller@ip-192-168-1-125.ec2.internal>
2026-04-04 10:35:27 -0700

    email

--- app/api/send-reminder/route.ts
+++ app/api/send-reminder/route.ts
@@ -29,6 +29,11 @@ export async function POST() {
 
   const milestones = [30, 14, 7, 3, 1, 0];
 
+  console.log("reminder:", reminder);
+  console.log("move.user.email:", move.user?.email);
+  console.log("daysLeft:", daysLeft);
+  console.log("move.lastReminderSent.toDateString:", move.lastReminderSent?.toDateString());
+
   if (
     reminder &&
     move.user?.email &&
