3774486442da90287765c8c91997e43bc17d9f58
Dalia Geller <daliageller@ip-192-168-1-125.ec2.internal>
2026-04-06 16:15:09 -0700

    dash

--- middleware.ts
+++ middleware.ts
@@ -12,12 +12,12 @@ export function middleware(req: NextRequest) {
       return NextResponse.redirect(new URL("/login", req.url));
     }
 
-    try {
-      jwt.verify(token, process.env.JWT_SECRET as string);
-    } catch {
-      return NextResponse.redirect(new URL("/login", req.url));
-    }
-  }
+    // try {
+    //   jwt.verify(token, process.env.JWT_SECRET as string);
+    // } catch {
+    //   return NextResponse.redirect(new URL("/login", req.url));
+    // }
+    // }
 
   return NextResponse.next();
 }
