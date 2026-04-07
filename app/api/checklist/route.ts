213d112385203abe2561ba1f4fa44666ed8f8b54
Dalia Geller <daliageller@ip-192-168-1-125.ec2.internal>
2026-04-06 17:14:23 -0700

    dash

--- app/api/checklist/route.ts
+++ app/api/checklist/route.ts
@@ -1,21 +1,8 @@
 import { prisma } from "@/lib/prisma";
 import jwt from "jsonwebtoken";
 import { NextResponse } from "next/server";
-// import { getUserFromToken } from "@/lib/auth";
-// import { cookies } from "next/headers";
 
 export async function GET(req: Request) {
-
-/*
-  const token = cookies().get("token")?.value;
-  const user = getUserFromToken(token);
-  if (!user) {
-    return NextResponse.json(
-      { error: "Unauthorized" },
-      { status: 401 }
-    );
-  }
-*/
 
   try {
     const token = req.headers
